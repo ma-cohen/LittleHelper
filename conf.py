5b4dfdd08b86938e55ff2a47a8fae85c8fb4696c
macohen <matan.workspace@gmail.com>
2020-10-17 19:00:44 +0300

    Fixing env variable to nor use .env file

--- conf.py
+++ conf.py
@@ -1,11 +1,11 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
+DISCORD_TOKEN = 'DISCORD_TOKEN'
 
-load_dotenv()
+if find_dotenv():
+    load_dotenv()
 
-token = os.getenv('DISCORD_TOKEN')
-
-
-
-
+token = os.getenv(DISCORD_TOKEN)
+if token is None:
+    raise ValueError(f'No {DISCORD_TOKEN} environment variable found')
