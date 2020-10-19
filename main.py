fbef8a9edd6cce1f4e7b09334ad29a8847a1e5c2
macohen <matan.workspace@gmail.com>
2020-10-19 21:34:21 +0300

    Start integrating firebase into the project

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import conf
-
+import firebase_admin
+from firebase_admin import credentials,firestore
 from little_helper import bot
 from logger import initialize_logger
 
 
+
 def main():
-    initialize_logger()
-    bot.run(conf.token)
+    # initialize_logger()
+    # bot.run(conf.token)
+    cred = credentials.Certificate('firebase-sdk.json')
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    emp_ref = db.collection('jokes')
+    docs = emp_ref.stream()
+    for doc in docs:
+        print(doc.to_dict())
 
 
 if __name__ == '__main__':
